91bffd6418498f365c5a99a6f0680b9bc601f98e
Edmilson Chaves <edm.chaves@gmail.com>
2022-09-21 21:39:20 -0300

    logicals operators

---
+++ udemy/fundamentos/aritmeticos.js
@@ -0,0 +1,10 @@
+const [a, b, c, d, e] = [10, 15, 22, 52, 100]
+
+const soma = a + b + c + d + e
+const subtracao = b - a
+const multiplicacao = a * c
+const divisao = e / d
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao.toFixed(2), modulo)
+
