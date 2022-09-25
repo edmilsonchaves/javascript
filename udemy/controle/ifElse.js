7d5f0ed349b0bfdc747c58e1cf6da3fe6e62e62f
Edmilson Chaves <58867496+edmilsonchaves@users.noreply.github.com>
2022-09-25 18:31:51 -0300

    folder toDo

---
+++ udemy/controle/ifElse.js
@@ -0,0 +1,17 @@
+
+const imprimirNota = function(nota) {
+    if (nota >= 7) {
+        console.log(`Sua nota é ${nota}, APROVADO!`)
+    } else {
+        console.log(`Sua nota é ${nota}, REPROVADO!`)
+    }
+}
+
+
+
+imprimirNota(6)
+imprimirNota(10)
+
+
+
+
--- udemy/fundamentos/arrays.js
+++ udemy/fundamentos/arrays.js
@@ -16,3 +16,4 @@ console.log(valores)
 
 
 
+
