b616a3a3ffbde7ea463fd0d5f2ced9f58b05ceae
Edmilson Chaves <edm.chaves@gmail.com>
2022-09-30 23:23:36 -0300

    add buttons

--- toDo/js/script.js
+++ toDo/js/script.js
@@ -3,14 +3,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let tarefa = document.getElementById('task')
     let nova_tarefa = document.getElementById('new-task-add')
     let add_btn = document.querySelector('.add-task-btn')
+    let confirm = document.querySelector('.confirm')
+    
 
     add_btn.addEventListener('click', (event) => {
         event.preventDefault()
         nova_tarefa.innerHTML += tarefa.value + '<br><br>'
+        confirm.innerHTML += '<div class"confirm"><button class="done">✔</button><button class="cancel">✕</button></div></br>'
         tarefa.value = ''
-        tarefa.focus()
+        tarefa.focus()     
+
         
 
     })
         
 })
+
+
