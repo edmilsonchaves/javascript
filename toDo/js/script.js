2dd38051883ab063a3b5cde25be4aafed56f66ed
Edmilson Chaves <edm.chaves@gmail.com>
2022-10-10 21:03:44 -0300

    add todolist

--- toDo/js/script.js
+++ toDo/js/script.js
@@ -1,3 +1,7 @@
+let tarefas = []
+
+let contador = -1
+
 window.addEventListener('DOMContentLoaded', (event) => {    
 
     let tarefa = document.getElementById('task')
@@ -10,31 +14,49 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     add_btn.addEventListener('click', (event) => {
         event.preventDefault()
-
+        
     
         if (tarefa.value == '') {
             alert('Campo inválido, insira uma tarefa!')
         } else {
             let teste = nova_tarefa.innerHTML += tarefa.value + '<br><br>'
-        confirm.innerHTML += '<div class"confirm"><button class="done" onclick="taskDone()">✔</button><button class="cancel" onclick="delete()">✕</button></div></br>'
+        confirm.innerHTML += '<div class"confirm"><button class="done" onclick="taskDone()">✔</button><button class="cancel" onclick="deleteTask()">✕</button></div></br>'
+        tarefas.push(tarefa.value)
         tarefa.value = ''
+        contador++
         tarefa.focus()  
-        }
-        
-       
+        }       
 
     })
         
 })
 
 function taskDone() {
-    document.querySelector('#new-task-add').style.textDecoration = 'line-through'
-    document.querySelector('#new-task-add').style.color = 'grey'
-}
 
-function deleteTask() {
-    document.querySelector('#new-task-add').innerHTML = ''
+    let tsk = document.querySelector('#new-task-add').textContent
     
+    if (tsk == tarefas[contador]){
+        document.querySelector('#new-task-add').style.color = 'grey'
+        document.querySelector('#new-task-add').style.textDecoration = 'line-through'
+    } else {
+        document.querySelector('#new-task-add').style.textDecoration = 'none'
+        document.querySelector('#new-task-add').style.color = 'black'
+    }   
+    document.querySelector('#new-task-add').style.textDecoration = 'none'
+    document.querySelector('#new-task-add').style.color = 'black'
+
+    }
+    
+
+function deleteTask() {    
+    
+    if (document.querySelector('#new-task-add').style.textDecoration == 'line-through') {
+        document.querySelector('#new-task-add').innerHTML = ''
+        document.querySelector('.confirm').innerHTML = ''
+    } else {
+        alert('A tarefa ainda não foi concluída!')
+
+    }
 
 }
 
