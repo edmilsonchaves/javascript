0888bb4ea6ed921e202d5103ea7f66041dda4b7c
Edmilson Chaves <58867496+edmilsonchaves@users.noreply.github.com>
2022-11-06 18:13:07 -0300

    novo curso js

---
+++ javascript_youtube/readme.md
@@ -0,0 +1,26 @@
+
+O QUE É JAVASCRIPT?
+
+* Uma linguagem de programação de alto nível;
+* Auxilia na criação de páginas, em conjunto com HTML e CSS;
+* Possui tipagem de dinâmica fraca;
+* É considerado multiparadigma (OO, imperativo, funcional);
+
+
+PARA QUE SERVE O JAVASCRIPT?
+
+* Manipulação de elementos e estilos na páginas (DOM);
+* Criar eventos em uma página.
+* Pode ser utilizado no back-end (Node.js);
+
+
+O QUE VOCÊ VAI APRENDER?
+
+* A diversas utilização do Javascript;
+* Sintaxe;
+* Tipos de dados;
+* Estruturas de condição e repetição;
+* Manipulação de DOM;
+* Eventos do Javascript;
+* Particularidades do Javascript;
+
--- memoria/js/script.js
+++ memoria/js/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front"
 const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
 let techs = [
     'bootstrap',
@@ -15,16 +17,88 @@ let techs = [
 ];
 
 
+let cards = null
+
+startGame()
+
+function startGame() {
+
+    cards = createCardsFromTechs(techs)
+    shuffleCards(cards)
+
+    initializeCards(cards);
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard")
+
+    cards.forEach((card) => {
+        
+        let cardElement = document.createElement('div')
+        cardElement.id = card.id
+        cardElement.classList.add('card')
+        cardElement.dataset.icon = card.icon
+
+        createCardContent(card, cardElement)
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement)
+    })
+
+}
+
+function createCardContent (card, cardElement) {
+
+    createCardFace(FRONT, card, cardElement)
+    createCardFace(BACK, card, cardElement)
+
+
+}
+
+function createCardFace (face, card, element) {
+
+    let cardElementFace = document.createElement('div')
+    cardElementFace.classList.add('face')
+
+    if (face === FRONT) {
+        
+        let iconElement = document.createElement('img')
+        iconElement.classList.add(ICON)
+        iconElement.src = "./images/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement)
+
+    }else {
+        cardElementFace.innerHTML = '&lt/&gt'
+    }
+    
+    element.appendChild(cardElementFace)
+
+
+}
+
+function shuffleCards(cards) {
+    let currentIndex = cards.length
+    let randomIndex = 0;
+
+    while (currentIndex !== 0 ) {
+
+        randomIndex  = Math.floor (Math.random * currentIndex)
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+    }
+}
+
 createCardsFromTechs(techs)
 
 function createCardsFromTechs(techs) {
 
     let cards = [];
 
-    for(let tech of techs) {
+    techs.forEach((tech) => {
         cards.push(createPairFromTech(tech));
 
-    }
+    })
     
     return cards.flatMap(pair => pair)
 
@@ -47,4 +121,9 @@ function createPairFromTech(tech) {
 function createdIdWithTech(tech) {
 
     return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard() {
+
+    this.classList.add('filp')
 }
