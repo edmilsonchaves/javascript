61498dc9db5c53d23532a662bf2376d77303532c
Edmilson de Jesus Chaves - External <e-edmilson@weg.net>
2022-11-17 11:03:36 -0300

    learn js

---
+++ js/javascript_youtube/22_funcoes/funcoes.js
@@ -0,0 +1,12 @@
+
+/*      FUNÇÕES
+
+    * Funções são blocos de códigos reutilizáveis;
+    * Ou seja, evitamos a repetição da lógica de um programa em diversas partes do código;
+    * A função precisa ser invocada para ser executada.
+    * Vamos ver na prática.
+
+*/
+
+
+
