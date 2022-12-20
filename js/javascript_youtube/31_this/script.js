eb9220894a90c051d370a40069b85cf514caa09e
Edmilson Chaves <58867496+edmilsonchaves@users.noreply.github.com>
2022-12-20 20:36:16 -0300

    this

---
+++ js/javascript_youtube/31_this/script.js
@@ -0,0 +1,11 @@
+
+
+/**
+ * A INSTRUÇÃO THIS
+ 
+ * Fora de escopo locais o "this" sempre se refere ao escopo global Window.
+ * Em objetos, o "this" vai se referir a instância e pode acessar suas propriedades;
+ * Vamos ver na prática;
+ */
+
+
