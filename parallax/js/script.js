1fc85a983aa21ed04c547dcdd9f84729e4e91eb3
edmilsonchaves <edm.chaves@gmaillcom>
2023-05-23 22:10:11 -0300

    parallax

---
+++ parallax/js/script.js
@@ -0,0 +1,14 @@
+let onda1 = document.getElementById('onda1');
+let onda2 = document.getElementById('onda2');
+let onda3 = document.getElementById('onda3');
+let onda4 = document.getElementById('onda4');
+
+window.addEventListener('scroll', function(){
+    let rolagemPos = window.scrollY
+
+    onda1.style.backgroundPositionX = 400 + rolagemPos * 4 + 'px'
+    onda2.style.backgroundPositionX = 400 + rolagemPos * -4 + 'px'
+    onda3.style.backgroundPositionX = 400 + rolagemPos * 2 + 'px'
+    onda4.style.backgroundPositionX = 400 + rolagemPos * -2 + 'px'
+})
+
